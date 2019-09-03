09735b28075ccc69280d6ce327dfcbf62ed673e3
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-09-03 10:39:02 -0700

    Agregar archivos de proyecto.

---
+++ GraficadorSeñales/SeñalExamen.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraficadorSeñales
+{
+    class SeñalExamen
+    {
+        public List<Muestra> Muestras { get; set; }
+        public SeñalExamen()
+        {
+            Muestras = 
+                new List<Muestra>();
+        }
+        public double evaluar(double tiempo)
+        {
+            double resultado;
+
+            resultado = 
+                tiempo * tiempo;
+
+            return resultado;
+        }
+    }
+}
